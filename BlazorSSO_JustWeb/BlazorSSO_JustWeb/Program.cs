5f47a00fdb725e5a8afb91b042d5c8530e330110
Ian Ferguson <ian.ferguson@kdi.kongsberg.com>
2023-07-30 13:34:56 +0200

    Added docker file, dockerrun script and a few notes to the project

--- BlazorSSO_JustWeb/BlazorSSO_JustWeb/Program.cs
+++ BlazorSSO_JustWeb/BlazorSSO_JustWeb/Program.cs
@@ -5,8 +5,16 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// when setting up oidc it would be nice to get the host address here, then we can configure oidc redirects for all deployments
+string clientBaseUrl = builder.HostEnvironment.BaseAddress;
+if (clientBaseUrl.EndsWith("/")) { clientBaseUrl = clientBaseUrl.TrimEnd('/'); }
+Console.Out.WriteLine("clientBaseUrl: " + clientBaseUrl);
 builder.Services.AddOidcAuthentication(options => {
-    builder.Configuration.Bind("Local", options.ProviderOptions);
+    builder.Configuration.Bind("GoogleSSO", options.ProviderOptions);
+    options.ProviderOptions.ClientId = "xxx"; // we want to get this from an environment variable, but how??
+    options.ProviderOptions.RedirectUri = clientBaseUrl + options.ProviderOptions.RedirectUri;
+    options.ProviderOptions.PostLogoutRedirectUri = clientBaseUrl + options.ProviderOptions.PostLogoutRedirectUri;
 });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
